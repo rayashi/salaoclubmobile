32cd2adc35b05ecb4198a8f370cf4fc192db54b4
rayashi <rayashi@gmail.com>
2020-02-07 17:12:05 -0300

    Added Auth page

---
+++ src/pages/auth/AuthReducer.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = {
+  token: null,
+  loading: false
+};
+
+export default (state = INITIAL_STATE, action) => {
+
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
